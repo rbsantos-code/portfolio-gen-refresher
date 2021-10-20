4076ac34b18b3879c5d57659006b217156e15399
rbsantos-code <richardbriansantos91@yahoo.com>
2021-10-19 18:51:10 -0700

    cleanup code

--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,6 @@
 
+const generatePage = (name, github) => {
+    return `
         <!DOCTYPE html> 
         <html lang="en"> 
         <head>
@@ -9,8 +11,12 @@
         </head>
     
         <body>
-        <h1>jack</h1>
-        <h2><a href="https://github.com/jackhub">Github</a></h2>
+        <h1>${name}</h1>
+        <h2><a href="https://github.com/${github}">Github</a></h2>
         </body>
         </html>
-        
+        `;
+};
+
+
+module.exports = generatePage;

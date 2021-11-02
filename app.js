04528ae15c9c81a506cc725d2a11cf10823fd696
rbsantos-code <richardbriansantos91@yahoo.com>
2021-11-01 19:54:39 -0700

    create and export generate site functions

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const inquirer = require('inquirer');
 
-
 const fs = require('fs');
 
 const generatePage = require('./src/page-template.js');
+
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 
 
@@ -110,19 +111,18 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile('./dist/index.html', pageHTML , err => {
-            if (err) throw err;
-
-            console.log('Portfolio complete! Check out index.html to see the output!');
-
-            fs.copyFile('./src/style.css', './dist/style.css', err => {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-                console.log('Style sheet copied successfully!');
-            });
-        }); 
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
